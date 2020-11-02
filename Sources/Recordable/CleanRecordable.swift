2c45649b82c5faba6f4d1ce4987fddf174d59d02
Vladislav Grigoryev <>
2020-11-02 13:46:09 +0300

    Greatly simplify clean recordable. Release 2.1.2.

--- Sources/Recordable/CleanRecordable.swift
+++ Sources/Recordable/CleanRecordable.swift
@@ -24,8 +24,62 @@
 //  THE SOFTWARE.
 
 import Foundation
+import ARKit
+
+private var cleanKey: UInt8 = 0
 
 public protocol CleanRecordable: AnyObject {
 
   var cleanPixelBuffer: CVPixelBuffer? { get }
+}
+
+public extension CleanRecordable {
+
+  internal var cleanStorage: AssociatedStorage<Clean<Self>> {
+    AssociatedStorage(object: self, key: &cleanKey, policy: .OBJC_ASSOCIATION_RETAIN)
+  }
+
+  internal var _clean: Clean<Self> {
+    get {
+      cleanStorage.get() ?? {
+        let clean = Clean(cleanRecordable: self)
+        _clean = clean
+        return clean
+      }()
+    }
+    set { cleanStorage.set(newValue) }
+  }
+
+  var clean: SelfRecordable { _clean }
+}
+
+final class Clean<T: CleanRecordable>: SelfRecordable {
+
+  public var recorder: (BaseRecorder & Renderable)? { cleanRecorder }
+
+  var cleanRecorder: CleanRecorder<T>?
+
+  var videoRecording: VideoRecording?
+
+  weak var cleanRecordable: T?
+
+  init(cleanRecordable: T) {
+    self.cleanRecordable = cleanRecordable
+  }
+
+  func injectRecorder() {
+    assert(cleanRecorder == nil)
+    assert(cleanRecordable != nil)
+
+    cleanRecorder = cleanRecordable.map { CleanRecorder($0) }
+  }
+
+  func prepareForRecording() {
+    (cleanRecordable as? SCNView)?.swizzle()
+    guard cleanRecorder == nil else { return }
+
+    injectRecorder()
+    assert(cleanRecorder != nil)
+    (cleanRecordable as? SCNView)?.addDelegate(cleanRecorder!)
+  }
 }
